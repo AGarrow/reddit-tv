a2ef0f1872beed8077aef51c160b48ac85871b5d
Alexi Garrow <agarrow@wealthsimple.com>
2020-12-13 18:32:04 -0500

    setup basic app

---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+// require("../styles/application.scss");
+
+// Render the top-level React component
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { RedditTV } from './RedditTV';
+
+ReactDOM.render(<RedditTV />, document.getElementById('root'));
