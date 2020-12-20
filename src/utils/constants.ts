033d74e19dbd51751d5fcc3cfdede9dcc9ad6113
Alexi Garrow <agarrow@wealthsimple.com>
2020-12-19 22:14:02 -0500

    add more channels

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,11 +1,29 @@
 export const DEFAULT_CHANNELS = [
   {
+    id: 'Cringe'
+  },
+  {
+    id: 'DeadOrVegetable'
+  },
+  {
+    id: 'Instant_regret'
+  },
+  {
+    id: 'Oops'
+  },
+  {
     id: 'PublicFreakout'
+  },
+  {
+    id: 'ThatLookedExpensive'
   },
   {
     id: 'WhatCouldGoWrong'
   },
   {
     id: 'WinStupidPrizes'
+  },
+  {
+    id: 'Videos'
   }
 ]
