c6603fc5fc138dcad466e180330d467567e1f92a
Alexi Garrow <agarrow@wealthsimple.com>
2020-12-24 17:05:03 -0500

    store current video index in redux store

--- src/components/channel/store/fetchVideos/videos.actions.ts
+++ src/components/channel/store/fetchVideos/videos.actions.ts
@@ -6,6 +6,10 @@ export const fetchVideos = (channelId, after) => async (dispatch, getState) => {
   dispatch({ type: 'channel/videos/videosLoaded', payload: response.data, id: channelId, after: after })
 }
 
+export const setCurrentVideoIndexAction = (channelId, index) => (dispatch, getState) => {
+  dispatch({ type: 'channel/videos/setIndex', channelId: channelId, index: index })
+}
+
 const redditURL = (channelId, after) => {
   const baseURL = `https://www.reddit.com/r/${channelId}/hot.json`
   if (after == null) {
--- src/components/channel/store/fetchVideos/videos.reducer.ts
+++ src/components/channel/store/fetchVideos/videos.reducer.ts
@@ -12,13 +12,23 @@ export const videosReducer = (state, action) => {
     case 'channel/videos/videosLoaded': {
       const returnedVideos = action.payload.data.children.filter((vid) => vid.data.is_video || vid.data.domain === 'youtube.com')
       const videoList = action.after == null ? returnedVideos : state[action.id].videos.concat(returnedVideos);
-      
+      const index = action.after == null ? 0 : state[action.id].currentVideoIndex
       return {
         ...state,
         [action.id]: {
           videos: videoList,
           after: action.payload.data.after,
           loading: false,
+          currentVideoIndex: index,
+        }
+      }
+    }
+    case 'channel/videos/setIndex': {
+      return {
+        ...state,
+        [action.channelId]: {
+          ...state[action.channelId],
+          currentVideoIndex: action.index
         }
       }
     }
